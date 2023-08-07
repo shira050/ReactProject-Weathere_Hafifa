d35cb3f5c67382d95edf8d23ac69623ee6f7de4f
shira <s0504173725@gmail..com>
2023-08-07 09:48:59 +0300

    save changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './comps/Home';
 import { Switch } from '@headlessui/react';
 import Main from './comps/main';
 import Haeder from './comps/haeder';
+import HistoryLastSearch from './comps/historyLastSearch';
 
 function App() {
   
@@ -18,7 +19,7 @@ function App() {
       <Routes>
           <Route index element={<Main />} />
           <Route path="/login" element={<Login />}/>        
-          <Route path="/history" element={<History />}/>        
+          <Route path="/history" element={<HistoryLastSearch />}/>        
           <Route path="*" element={()=>{return <h1>not found!!</h1>}} /> 
       </Routes>
     </BrowserRouter>
--- src/comps/haeder.jsx
+++ src/comps/haeder.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../comps/css/header.css' 
 
 export default function Haeder() {
   return (
-    <nav dir="rtl" class=" navbar bg-dark">
-      <div class="container-fluid">
-        <Link class="navbar-brand text-light" to='/'>ראשי</Link>
+    <nav dir="rtl" class=" d-flex justify-content-around bg-dark">
+      <div class="container-fluid ">
+        <Link class="navLink" to='/'>ראשי</Link> 
+        <Link class="navLink" to='/'>היסטוריה</Link>
       </div>
     </nav>
   )
