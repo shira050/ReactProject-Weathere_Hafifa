9131195e6a7946ba729b0dd2f2db49b10bbba694
shira <s0504173725@gmail..com>
2023-08-07 10:56:20 +0300

    merge

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
 import Home from './comps/Home';
 import { Switch } from '@headlessui/react';
 import Main from './comps/main';
+// import HistoryLastSearch from './comps/historyLastSearch';
+
 
 function App() {
   
@@ -13,9 +15,11 @@ function App() {
 
      <div style={{direction:'rtl'}}>
      <BrowserRouter>
+     <Haeder/>
       <Routes>
           <Route index element={<Main />} />
-          <Route path="/login" element={<Login />}/>        
+          <Route path="/login" element={<Login />}/>  
+          {/* <Route path="/history" element={<HistoryLastSearch />}/>               */}
           <Route path="*" element={()=>{return <h1>not found!!</h1>}} /> 
       </Routes>
     </BrowserRouter>
