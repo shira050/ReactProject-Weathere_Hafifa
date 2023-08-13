ed3df102647d332f14687a151b181d94c960fb23
shira <s0504173725@gmail..com>
2023-08-13 08:08:39 +0300

    add history count

--- src/comps/haeder.jsx
+++ src/comps/haeder.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import '../comps/css/header.css' 
+import { CityContext } from '../context/cityContext';
 
 export default function Haeder() {
+  const { historySearch} = useContext(CityContext);
+
   return (
     <nav class=" d-flex justify-content-around bg-dark">
       <div class="container-fluid p-3">
         <Link class="navLink" to='/'>ראשי</Link> 
-        <Link class="navLink" to='/history'>היסטוריה</Link>
+        <Link class="navLink" to='/history'>היסטוריה-{historySearch.length}</Link>
       </div>
     </nav>
   )
