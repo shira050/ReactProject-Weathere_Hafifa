f4815f431d2517f5471440742f5dd34750896ba4
shira <s0504173725@gmail..com>
2023-08-20 12:11:33 +0300

    change header to ask on localUser

--- src/comps/weathere/haeder.jsx
+++ src/comps/weathere/haeder.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import '../css/header.css'
-import { UserContext } from '../../context/userContext';
+import { USER } from '../../services/apiBasic';
 
 export default function Haeder() {
-  const { currentUser } = useContext(UserContext);
-
+  let localUser;
+  if(localStorage[USER]){
+    localUser= JSON.parse(localStorage[USER]);
+}
   return (
     <nav dir="rtl" class="bg-dark p-2">
       <div class="container-fluid">
         <Link class="navLink" to='/'>ראשי</Link>
-       {currentUser&& <Link class="navLink" to='/mador'>מדור</Link>}
+       {localUser&& <Link class="navLink" to='/mador'>מדור</Link>}
       </div>
     </nav>
   )
