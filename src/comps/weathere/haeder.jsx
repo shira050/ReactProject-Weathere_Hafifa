1e65e7d6a28a08732a5cc9c22d7824a099771213
shira <s0504173725@gmail..com>
2023-08-27 08:33:21 +0300

    commit

--- src/comps/weathere/haeder.jsx
+++ src/comps/weathere/haeder.jsx
@@ -1,19 +1,46 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import '../css/header.css'
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../css/header.css';
 import { USER } from '../../services/apiBasic';
 
-export default function Haeder() {
+export default function Header() {
   let localUser;
-  if(localStorage[USER]){
-    localUser= JSON.parse(localStorage[USER]);
-}
+  const nav = useNavigate();
+  if (localStorage[USER]) {
+    localUser = JSON.parse(localStorage[USER]);
+  }
+  const onLogOut = () => {
+    if (window.confirm('האם אתה בטוח שברצונך להתנתק?')) {
+      localStorage.removeItem(USER);
+      nav('/');
+    }
+  };
+
   return (
-    <nav dir="rtl" class="bg-dark p-2">
-      <div class="container-fluid">
-        <Link class="navLink" to='/'>ראשי</Link>
-       {localUser&& <Link class="navLink" to='/mador'>מדור</Link>}
+    <nav className="bg-dark p-2 container-fluid">
+      <div className="container d-flex justify-content-between"> {/* Use justify-content-between here */}
+        <div className="text-start">
+          <Link className="navLink" to="/">
+            ראשי
+          </Link>
+          {localUser && (
+            <Link className="navLink" to="/mador">
+              מדור
+            </Link>
+          )}
+        </div>
+        <div className="text-end">
+          {localUser && (
+            <Link
+              className="navLink"
+              to="/"
+              onClick={() => onLogOut()}
+            >
+              התנתקות
+            </Link>
+          )}
+        </div>
       </div>
     </nav>
-  )
+  );
 }
