9411bff957348bc21f7c4c239998a3834213a9ef
shira <s0504173725@gmail..com>
2023-08-30 10:24:52 +0300

    update login to ant

--- src/comps/login.jsx
+++ src/comps/login.jsx
@@ -1,12 +1,16 @@
+
+
+
+
 import React, { useContext, useState } from 'react';
-import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBIcon } from 'mdb-react-ui-kit';
-import './css/login.css';
+import { Button, Card, Input } from 'antd';
+import './css/login.css'; 
 import { doApiLogin } from '../services/apiService';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/userContext';
 
 function Login() {
-    const [userName, setuserName] = useState('');
+    const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState({});
     const { currentUser, updateUser } = useContext(UserContext);
@@ -16,7 +20,6 @@ function Login() {
         name: userName,
         password: password
     }
-
     const validateForm = () => {
         const errors = {};
 
@@ -34,98 +37,66 @@ function Login() {
 
         setErrors(errors);
 
-        return Object.keys(errors).length === 0; 
+        return Object.keys(errors).length === 0;
     };
 
     const login = async () => {
         const isValid = validateForm();
 
         if (isValid) {
-            
-           let res= await doApiLogin(user);           
-           if(res&&res.status==200){
-               localStorage.setItem('user', JSON.stringify(user));
-               updateUser(res.data);
-           alert('Welcome ' + userName);
-           nav('/');
-           }
-           else if(!res){
-            alert("יש לנו שגיאה בשרת נסה מאוחר יותר....")
-           }
-           else{
-               alert("אחד או יותר מהפרטים שגוי...")
-           }
+
+            let res = await doApiLogin(user);
+            if (res && res.status === 200) {
+                localStorage.setItem('user', JSON.stringify(user));
+                updateUser(res.data);
+                alert('Welcome ' + userName);
+                nav('/');
+            }
+            else if (!res) {
+                alert("יש לנו שגיאה בשרת נסה מאוחר יותר....")
+            }
+            else {
+                alert("אחד או יותר מהפרטים שגוי...")
+            }
         }
-    };
+    }
 
     return (
-        <MDBContainer fluid>
-            <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-                <MDBCol col='12'>
-                    <MDBCard className='bg-dark text-white my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px' }}>
-                        <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
-                            <h2 className='fw-bold mb-2 text-uppercase'>Login</h2>
-                            <p className='text-white-50 mb-5'>Please enter your name and password!</p>
+        <div className="login-container">
+            <Card className="login-card">
+                <div className="login-card-content">
+                    <h2 className="login-header">Login</h2>
+                    <p className="login-subheader">Please enter your name and password!</p>
 
-                            {errors.userName && <div className='text-danger'>{errors.userName}</div>}
+                    <Input
+                        className="mb-5"
+                        placeholder="userName"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    {errors.userName && <div className="error-message">{errors.userName}</div>}
 
-                            <MDBInput
-                                wrapperClass='mb-4 mx-5 w-100'
-                                labelClass='text-white'
-                                label='userName '
-                                id='formControlLg'
-                                type='text'
-                                size='lg'
-                                value={userName}
-                                onChange={(e) => setuserName(e.target.value)}
-                                error={errors.userName}
-                            />
+                    <Input.Password
+                        className="mb-5"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {errors.password && <div className="error-message">{errors.password}</div>}
 
-                            {errors.password && <div className='text-danger'>{errors.password}</div>}
+                    <Button type="primary" size="large" onClick={login} style={{ width: '100px', alignSelf: 'center' }}>
+                        Login
+                    </Button>
 
-                            <MDBInput
-                                wrapperClass='mb-4 mx-5 w-100'
-                                labelClass='text-white'
-                                label='Password'
-                                id='formControlLg'
-                                type='password'
-                                size='lg'
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                error={errors.password}
-                            />
 
-                            <button outline className='mx-2 px-5' color='white' size='lg' onClick={login}>
-                                Login
-                            </button>
-
-                            {/* icons for social media */}
-                            <div className='d-flex flex-row mt-3 mb-5'>
-                                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                                    <MDBIcon fab icon='facebook-f' size='lg' />
-                                </MDBBtn>
-
-                                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                                    <MDBIcon fab icon='twitter' size='lg' />
-                                </MDBBtn>
-
-                                <MDBBtn tag='a' color='none' className='m-3' style={{ color: 'white' }}>
-                                    <MDBIcon fab icon='google' size='lg' />
-                                </MDBBtn>
-                            </div>
-                            {/* end icons for social media */}
-
-                            <div>
-                                <p className='mb-0'>
-                                    Don't have an account? <a href='#!' className='text-white-50 fw-bold'>Sign Up</a>
-                                </p>
-                            </div>
-                        </MDBCardBody>
-                    </MDBCard>
-                </MDBCol>
-            </MDBRow>
-        </MDBContainer>
+                    <div className="sign-up-link">
+                        <p>Don't have an account? <a href="#!">Sign Up</a></p>
+                    </div>
+                </div>
+            </Card>
+        </div>
     );
-}
+    }
 
-export default Login;
+    export default Login;
+
