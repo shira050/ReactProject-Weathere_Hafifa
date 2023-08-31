d4ef6b7723a56a998042c31a7ab750ed3e218edd
shira <s0504173725@gmail.com>
2023-08-31 09:23:47 +0300

    add error page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './comps/login';
-
-import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'; 
 import Main from './comps/main';
 import Haeder from './comps/weathere/haeder';
 import Mador from './comps/mador/mador';
 import './App.css';
+import { USER } from './services/apiBasic';
+import Error404Page from './comps/404Page';
 
 function App() {
-  
-  return (
+  let currentUser;
+  if (localStorage[USER]) {
+    currentUser = JSON.parse(localStorage[USER]);
+  }
 
-     <>
-     <BrowserRouter>
-     <Haeder/>
-      <Routes>
+  return (
+    <>
+      <BrowserRouter>
+        <Haeder />
+        <Routes>
           <Route index element={<Main />} />
-          <Route path="/login" element={<Login />}/>        
-          <Route path="/mador" element={<Mador />}/>        
-          <Route path="*" element={()=>{return <h1>not found!!</h1>}} /> 
-      </Routes>
-    </BrowserRouter>
-     </>
+          <Route path="/login" element={<Login />} />
+          {currentUser && <Route path="/mador" element={<Mador />} />}
+          <Route path="*" element={<Error404Page/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
